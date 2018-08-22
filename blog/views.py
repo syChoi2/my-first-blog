609bb7e0c15deada36473b1ccec91780fd89340a
minamcra <minamcra@gmail.com>
2018-08-22 13:45:27 +0900

    template apply

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,6 +6,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
 	#render(요청, template, 매개변수)
+	
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk = post_id)
+    return render(request, 'blog/post_detail.html', {'post' : post})
 
 
 
